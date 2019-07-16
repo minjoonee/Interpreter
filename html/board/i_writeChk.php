00ba80bcd1d6486079233d5d24ec133e549df719
Song-gicheon <38076429+Song-gicheon@users.noreply.github.com>
2019-07-16 10:28:29 +0900

    Update i_writeChk.php

--- html/board/i_writeChk.php
+++ html/board/i_writeChk.php
@@ -17,7 +17,7 @@ $db_result2 = $_POST['result2'];
 else{
 	$db_id= $_SESSION['id'];
 
-  $conn=mysqli_connect('127.0.0.1', 'inter', '771029', 'interpreter', '3306');
+  $conn=mysqli_connect('localhost', 'db_id', 'db_pw', 'db_name', '3306');
     if(!$conn){
         echo"connect fail....";
     }
